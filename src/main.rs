48b3bd4f9ea0eac817b103a95432b393e7d4d10e
leegeunhyeok <dev.ghlee@gmail.com>
2022-04-17 21:24:58 +0900

    feat: 1.3 first implementation

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,14 @@ struct Cli {
 
 fn main() {
   let args = Cli::parse();
+  let content = std::fs::read_to_string(&args.path)
+    .expect("could not read file");
+
   println!("pattern: {}", args.pattern);
+
+  for line in content.lines() {
+    if line.contains(&args.pattern) {
+      println!("{}", line);
+    }
+  }
 }
