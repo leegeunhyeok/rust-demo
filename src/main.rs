406330eeafdc14ff242b8e376d5cd489aea4115c
leegeunhyeok <dev.ghlee@gmail.com>
2022-04-24 10:05:46 +0900

    feat: 1.6 testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 
 use std::{thread, time};
+use std::process::Command; // Run programs
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
 use anyhow::{Context, Result};
 use clap::Parser;
 use indicatif::ProgressBar;
@@ -30,5 +33,17 @@ fn main() -> Result<()>  {
 
   println!("pattern: {}", args.pattern);
   println!("{}", content);
+  Ok(())
+}
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+  let mut cmd = Command::cargo_bin("rst")?;
+
+  cmd.arg("foobar").arg("test/file/doesnt/exist");
+  cmd.assert()
+    .failure()
+    .stderr(predicate::str::contains("could not read file"));
+
   Ok(())
 }
